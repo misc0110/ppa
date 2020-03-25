b98c9459e089c2411abc7f6f2308cd94e5ea68f1
Michael Schwarz <michael.schwarz91@gmail.com>
2020-03-25 22:56:47 +0100

    ..

--- dists/focal/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dpkg-scanpackages --multiversion . > Packages
-gzip -k -f Packages
-apt-ftparchive release . > Release
-gpg --default-key 191001FCD1A0490FC486BE4886D138BA406892E8 -abs -o - Release > Release.gpg
-gpg --default-key 191001FCD1A0490FC486BE4886D138BA406892E8 --clearsign -o - Release > InRelease
-echo "deb https://misc0110.github.io/ppa ./" > file.list
-
